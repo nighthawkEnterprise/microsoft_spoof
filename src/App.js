355b843b8e1f656ee6a2877a948f04879dd9171e
Nithin Kumar Moorthy <nithinmoorthy11@gmail.com>
2021-03-08 22:01:03 -0800

    Initial Concept Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import microsoft from './microsoft.jpg';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className="container">
+        <div className="login">
+          <div className='header'>
+            <img src={microsoft} height={50} width={100}/>
+            <h2> Sign in </h2>
+          </div>
+          <input type="email" placeholder="Email, phone, or Skype"/>
+          <label><p> No account?   <a href="#">     Create one! </a></p></label>
+          <div id="windows-hello"> 
+          <label><p> <a href="#"> Sign in with Windows Hello or a security key</a> </p></label>
+          <img id="question" src="https://logincdn.msauth.net/shared/1.0/content/images/documentation_bcb4d1dc4eae64f0b2b2538209d8435a.svg"/>
+          </div>
+          <div class="btn-container">
+            <input type="submit" class="button" value="Next"/>
+          </div>
+        </div>
+        <div className="sign-in">
+          <img id="key" src="https://logincdn.msauth.net/shared/1.0/content/images/signin-options_4e48046ce74f4b89d45037c90576bfac.svg" />
+          <p id="sign"> Sign-in options </p>
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
